8c2a522b0f8a501018ce8324b748da084e8fbebe
ediaz13 <diazemanuel27@gmail.com>
2024-10-16 00:40:34 -0300

    Top FindDuplicateSubtrees

--- interview Practice/src/hashtables/findingduplicates/FindDuplicateSubtrees.java
+++ interview Practice/src/hashtables/findingduplicates/FindDuplicateSubtrees.java
@@ -2,7 +2,50 @@ package hashtables.findingduplicates;
 
 public class FindDuplicateSubtrees {
 
+    public void findDuplicateSubtrees() {
+
+    }
+
+    // Method to insert nodes in the binary tree
+    public TreeNode insert(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insert(root.left, val);
+        } else if (val > root.val) {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    // Method to print the binary tree (in-order traversal)
+    public void inOrder(TreeNode root) {
+        if (root != null) {
+            inOrder(root.left);
+            System.out.print(root.val + " ");
+            inOrder(root.right);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("FindDuplicateSubtrees");
+        FindDuplicateSubtrees tree = new FindDuplicateSubtrees();
+        TreeNode root = null;
+        int[] values = {5, 3, 8, 1, 4, 7, 9};
+
+        for (int val : values) {
+            root = tree.insert(root, val);
+        }
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
     }
 }
